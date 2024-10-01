87f4ba3113003934acf31aedd0542041741f2c0c
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2024-09-30 23:23:07 -0600

    update navigate

--- Frontend/src/app/layouts/full/sidebar/sidebar-data.ts
+++ Frontend/src/app/layouts/full/sidebar/sidebar-data.ts
@@ -13,16 +13,6 @@ export const navItems: NavItem[] = [
     navCap: 'User Panel',
   },
   {
-    displayName: 'Albums',
-    iconName: 'photo',
-    route: '/ui-components/badge',
-  },
-  {
-    displayName: 'Edit Albums',
-    iconName: 'settings',
-    route: '/ui-components/chips',
-  },
-  {
     displayName: 'Load Images',
     iconName: 'refresh',
     route: '/ui-components/lists',
@@ -31,10 +21,7 @@ export const navItems: NavItem[] = [
     displayName: 'Analyze Text',
     iconName: 'layout-navbar-expand',
     route: '/ui-components/menu',
-  },
-  {
-    displayName: 'Tooltips',
-    iconName: 'tooltip',
-    route: '/ui-components/tooltips',
   }
 ];
+
+
